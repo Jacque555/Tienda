bd436481f82fafbb4e7b543ca3605c2081950de9
Jacqueline <guadalupehe12@gmail.com>
2022-09-17 15:16:47 -0500

    git inicial

---
+++ Base de datos Tienda/Tienda.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE Tienda;
+USE Tienda;
+
+CREATE TABLE producto(
+idproducto INT PRIMARY KEY AUTO_INCREMENT,
+nombre VARCHAR(100),
+descripcion VARCHAR(200),
+precio DOUBLE); 
+
---
+++ PresentacionTienda/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PresentacionTienda
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new FrmTienda());
+        }
+    }
+}
